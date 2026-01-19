a7813d245d7d4acafd0518d4b8199455f33481fd
Lohith-LM <lohith.lm2006@gmail.com>
2026-01-19 19:00:55 +0530

    Add loops example

---
+++ loops.py
@@ -0,0 +1,9 @@
+# Print numbers from 1 to 10 using a for loop
+for i in range(1, 11):
+    print(i)
+
+# Print numbers from 10 to 1 using a while loop
+j = 10
+while j >= 1:
+    print(j)
+    j -= 1
