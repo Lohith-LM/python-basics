6f41e1e1cec0d4c32d62124cfa680af054be530a
LOHITH L M <lohith.lm2006@gmail.com>
2026-01-21 18:50:18 +0530

    Add lists example

---
+++ lists.py
@@ -0,0 +1,11 @@
+# Creating a list
+numbers = [1, 2, 3, 4, 5]
+# Accessing elements
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+# Adding an element
+numbers.append(6)
+print("After adding 6:", numbers)
+# Looping through a list
+for num in numbers:
+    print(num)
