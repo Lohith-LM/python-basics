210bb2aad5bad8a00eb05da576adbee3b84d7fbb
LOHITH L M <lohith.lm2006@gmail.com>
2026-02-04 21:14:14 +0530

    Add lists and dictionaries example

---
+++ lists_dicts.py
@@ -0,0 +1,12 @@
+# List Example
+numbers = [1, 2, 3, 4, 5]
+print("Numbers:", numbers)
+
+# Dictionary Example
+student = {
+    "name": "Lohith",
+    "branch": "AIML",
+    "year": 2
+}
+
+print("Student Name:", student["name"])
